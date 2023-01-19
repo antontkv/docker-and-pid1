a59ff6692521a9d9096a7160a9f4fdca60294324
Anton Tuchkov <to.tuchkov@gmail.com>
2023-01-19 11:30:49 +0500

    Run script that don't handle SIGTER inside Docker container without init

--- run.sh
+++ run.sh
@@ -36,11 +36,26 @@ python_term_bare() {
     rm ./term_wait.log
 }
 
+python_noterm_docker_noinit() {
+    echo "[+] Launching python script in Docker container"
+    docker run -d -v $PWD/noterm_wait.py:/noterm_wait.py --rm --name python_noinit python:3.11.1-alpine python3 /noterm_wait.py
+    sleep 1
+    echo "[+] ps command output in container:"
+    echo ""
+    docker exec -it python_noinit ps -ef
+    echo "[+] Stopping docker container and timing it. If it takes more then 10 seconds, it means"
+    echo "    that docker killing the process."
+    time docker stop python_noinit
+}
+
 case $1 in
     "python_noterm_bare" )
         python_noterm_bare;
         ;;
     "python_term_bare" )
         python_term_bare;
+        ;;
+    "python_noterm_docker_noinit" )
+        python_noterm_docker_noinit;
         ;;
 esac
