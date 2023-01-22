1f309d42f72aa9168ce6e5e98b8f284ee6d7b018
Anton Tuchkov <to.tuchkov@gmail.com>
2023-01-22 12:09:22 +0500

    Add colors to log output

--- run.sh
+++ run.sh
@@ -1,65 +1,68 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 python_noterm_bare() {
-    echo "[+] Launching python script in background"
+    echo -e "$GREEN[+] Launching python script in background$NC"
     ./noterm_wait.py &
     child_pid=$!
-    echo "[+] PID of python script: $child_pid"
-    echo "[+] Sending SIGTERM to PID $child_pid"
+    echo -e "$GREEN[+] PID of python script: $child_pid$NC"
+    echo -e "$GREEN[+] Sending SIGTERM to PID $child_pid$NC"
     kill -s TERM $child_pid
     if ps -p $child_pid > /dev/null
     then
-        echo "[!] PID $child_pid is still running"
+        echo -e "$RED[!] PID $child_pid is still running$NC"
     else
-        echo "[!] PID $child_pid is not running"
+        echo -e "$GREEN[+] PID $child_pid is not running$NC"
     fi
 }
 
 python_term_bare() {
-    echo "[+] Launching python script in background"
+    echo -e "$GREEN[+] Launching python script in background$NC"
     ./term_wait.py > ./term_wait.log &
     sleep 1
     child_pid=$!
-    echo "[+] PID of python script: $child_pid"
-    echo "[+] Sending SIGTERM to PID $child_pid"
+    echo -e "$GREEN[+] PID of python script: $child_pid$NC"
+    echo -e "$GREEN[+] Sending SIGTERM to PID $child_pid$NC"
     kill -s TERM $child_pid
     sleep 1
     if ps -p $child_pid > /dev/null
     then
-        echo "[!] PID $child_pid is still running"
+        echo -e "$RED[!] PID $child_pid is still running$NC"
     else
-        echo "[!] PID $child_pid is not running"
+        echo -e "$GREEN[+] PID $child_pid is not running$NC"
     fi
-    echo ""
-    echo "PID $child_pid process stdout:"
+    echo -e "$GREEN[+] PID $child_pid process stdout:$NC"
     echo ""
     cat ./term_wait.log
     rm ./term_wait.log
 }
 
 python_noterm_docker_noinit() {
-    echo "[+] Launching python script in Docker container"
+    echo -e "$GREEN[+] Launching python script in Docker container$NC"
     docker run -d -v $PWD/noterm_wait.py:/noterm_wait.py --rm --name python_noinit python:3.11.1-alpine python3 /noterm_wait.py
     sleep 1
-    echo "[+] ps command output in container:"
+    echo -e "$GREEN[+] ps command output in container:$NC"
     echo ""
     docker exec -it python_noinit ps -ef
-    echo "[+] Stopping docker container and timing it. If it takes more then 10 seconds, it means"
-    echo "    that docker killing the process."
+    echo -e "$GREEN[+] Stopping docker container and timing it. If it takes more then 10 seconds, it means"
+    echo -e "    that docker killing the process.$NC"
     time docker stop python_noinit
 }
 
 python_term_docker_noinit() {
-    echo "[+] Launching python script in Docker container"
+    echo -e "$GREEN[+] Launching python script in Docker container$NC"
     docker run -d -v $PWD/term_wait.py:/term_wait.py --name python_noinit python:3.11.1-alpine python3 /term_wait.py
     sleep 1
-    echo "[+] ps command output in container:"
+    echo -e "$GREEN[+] ps command output in container:$NC"
     echo ""
     docker exec -it python_noinit ps -ef
-    echo "[+] Stopping docker container and timing it. If it takes more then 10 seconds, it means"
-    echo "    that docker killing the process."
+    echo -e "$GREEN[+] Stopping docker container and timing it. If it takes more then 10 seconds, it means"
+    echo -e "    that docker killing the process.$NC"
     time docker stop python_noinit > /dev/null
-    echo "[+] Container logs:"
+    echo -e "$GREEN[+] Container logs:$NC"
     echo ""
     docker logs python_noinit
     docker rm python_noinit > /dev/null
