1d99cb222355923b7b893f1caa39b20ef82ed947
Anton Tuchkov <to.tuchkov@gmail.com>
2023-01-22 16:54:55 +0500

    Print UUID for log line and flush first log line from script
    - UUID for distinguishing between repeating lines in the output
    - Flush is for printing first line right away and not when the script
      exits

--- term_wait.py
+++ term_wait.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
 import signal
-from types import FrameType
+import uuid
 from time import sleep
+from types import FrameType
+
 
 def wait_forever():
     while True:
         sleep(1)
 
 def handler(signum: int, frame: FrameType):
-    print(f"Received SIGTERM. Exiting.")
+    print(f"[{uuid.uuid4().hex}] Received SIGTERM. Exiting.")
     exit(0)
 
 signal.signal(signal.SIGTERM, handler)
 
 if __name__ == "__main__":
-    print("Started process.")
+    print("Started process.", flush=True)
     wait_forever()
