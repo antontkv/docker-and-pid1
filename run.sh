8fe4abb3ba1de04193ed8ee1b1c3fdd54379fa0e
Anton Tuchkov <to.tuchkov@gmail.com>
2023-01-22 12:17:23 +0500

    Running script that don't handel SIGTERM inside Docker with init system

--- run.sh
+++ run.sh
@@ -68,6 +68,18 @@ python_term_docker_noinit() {
     docker rm python_noinit > /dev/null
 }
 
+python_noterm_docker_init() {
+    echo -e "$GREEN[+] Launching python script in Docker container with init as PID 1$NC"
+    docker run -d -v $PWD/noterm_wait.py:/noterm_wait.py --rm --init --name python_init python:3.11.1-alpine python3 /noterm_wait.py
+    sleep 1
+    echo -e "$GREEN[+] ps command output in container:$NC"
+    echo ""
+    docker exec -it python_init ps -ef
+    echo -e "$GREEN[+] Stopping docker container and timing it. If it takes more then 10 seconds, it means"
+    echo -e "    that docker killing the process.$NC"
+    time docker stop python_init
+}
+
 case $1 in
     "python_noterm_bare" )
         python_noterm_bare;
@@ -80,5 +92,8 @@ case $1 in
         ;;
     "python_term_docker_noinit" )
         python_term_docker_noinit;
+        ;;
+    "python_noterm_docker_init" )
+        python_noterm_docker_init;
         ;;
 esac
